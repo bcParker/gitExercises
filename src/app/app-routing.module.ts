cd06b523d890ccdfa12758ac7e5f171cb3a71078
bcParker <cparker8890@gmail.com>
2019-08-10 11:01:13 -0400

    added Practice component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PracticeComponent } from './practice/practice.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'practice', component: PracticeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
