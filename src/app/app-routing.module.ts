e960c5d9b09b9a0138e882c0b94a86d37083671f
Adam Kern <kern31@sbcglobal.net>
2019-08-10 11:01:49 -0400

    ak first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdamComponent } from '../app/adam/adam.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'adam', component: AdamComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
